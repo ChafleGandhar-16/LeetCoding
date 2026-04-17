328b0ef5200043083c0d41a7f1b8c9669f6c42e3
GANDHAR CHAFLE <gandharchafle@gmail.com>
2026-04-17 22:01:45 +0530

    Time: 3 ms (60.04%), Space: 44.3 MB (67.78%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -2,19 +2,6 @@ import java.util.*;
 
 class Solution {
     public boolean isPalindrome(String s) {
-        // int k = 0;
-        // for (int i = 0; i<s.length(); i++) {
-        //     if (Character.isLetter(s.charAt(i))) {
-        //         s.replace(s.charAt(k), s.charAt(i));
-        //         k++;
-        //     }
-        // }
-        // s = s.toLowerCase();
-        // System.out.println(s);
-        // for (int i = 0; i<k/2; i++) {
-        //     if (s.charAt(i)!=s.charAt(k-i)) return false;
-        // }
-        // return true;
         if (s.length() == 1 && Character.isLetterOrDigit(s.charAt(0))) return true;
         int left = 0;
         int right = s.length()-1;
